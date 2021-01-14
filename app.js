afb5bc8ed32e0ce4f027025ba86fba8cfe40f4a3
maxly20 <maxly2011@gmail.com>
2021-01-14 15:43:58 +1100

    refactor code

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function nextSlide() {
   slides[i].classList.remove('active');
   i = (i + 1) % slides.length;
   slides[i].classList.add('active');
+  
 }
 function prevSlide() {
   slides[i].classList.remove('active');
